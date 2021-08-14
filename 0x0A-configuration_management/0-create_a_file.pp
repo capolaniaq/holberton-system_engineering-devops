34475c919ce7fcd1c253fd35ca73fa76e011dd1c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-13 19:19:51 -0500

    fixing the owner and group

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # create file holberton in a folder tmp, with these requeriments above
 
-$str = 'I love Puppet'
 
 file {'holberton':
-  path => '/tmp/holberton',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => $str,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet',
 }
