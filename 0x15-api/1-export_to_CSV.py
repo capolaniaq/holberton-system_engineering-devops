03812d3044900e525961f912d152c00e7a7bb12e
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-13 17:22:37 -0500

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+returns information about his/her TODO list progress.
+and generate the file.csv"""
 
 import csv
 import requests
