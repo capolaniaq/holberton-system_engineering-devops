8d7d19df833c7b421062baa5ee5a3d2152ecc67a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-27 19:05:26 -0500

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,8 @@ def top_ten(subreddit):
     """Function that pint the top 10 hot post in a subreddit"""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'MyBot/0.0.1'}
-    res = get(url, headers=headers)
+    res = get(url, headers=headers, allow_redirects=False)
+
     if res.status_code != 200:
         print(None)
         return 0
@@ -18,4 +19,4 @@ def top_ten(subreddit):
         print(post['data']['title'])
         i += 1
         if i == 9:
-            break
+            return 0
