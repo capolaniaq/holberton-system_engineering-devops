7dc7ab249d9a699a4b1298626250fb17c98e7a29
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-29 18:31:33 -0500

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,8 +3,3 @@ exec { 'fix-phpp':
   command  => 'sed -i s/.phpp/.php/g /var/www/html/wp-settings.php',
   path     => ['/bin', '/usr/bin', '/usr/sbin']
 }
-# wordpress apache2 running again
-exec { 'fixing-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
-}
