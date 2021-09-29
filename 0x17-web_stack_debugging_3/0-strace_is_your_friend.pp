7208948b2e0ca276cf5aeee1445f4961522a3124
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-29 18:26:29 -0500

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# wordpress apache2 running again
-exec { 'fixing-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+# fix 500 server error
+exec { 'fix-phpp':
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path     => 'usr/bin/:/bin/',
 }
