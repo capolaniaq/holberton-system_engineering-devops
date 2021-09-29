2103aa6ae999ead8b3e6d587d1b14ff36af2308c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-29 18:12:43 -0500

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix 500 server error
 exec { 'fix-phpp':
+  command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path     => 'usr/bin/:/bin/',
-  command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",                    provider => 'shell',                       
 }
 
