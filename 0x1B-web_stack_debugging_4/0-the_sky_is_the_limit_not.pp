096b0f80963f7d17b2c08578e10d97c2e8d3297c
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-10-07 22:35:28 -0500

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,6 @@
 exec { 'limit higher':
   environment => ['old=ULIMIT="-n 15"', 'new=ULIMIT="-n 65535"'],
   command     => 'sed -i s/old/new/g /etc/default/nginx; sudo service nginx restart',
-  path        => 'usr/bin/:/bin/'
+  path        => 'usr/bin/:/bin/',
+  returns     => [0, 1]
 }
