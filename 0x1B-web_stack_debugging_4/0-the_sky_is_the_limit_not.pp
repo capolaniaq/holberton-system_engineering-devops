771c2b5b7302215200b785839b851ff321889921
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-10-07 22:34:02 -0500

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# server error 500
+exec { 'limit higher':
+  environment => ['old=ULIMIT="-n 15"', 'new=ULIMIT="-n 65535"'],
+  command     => 'sed -i s/old/new/g /etc/default/nginx; sudo service nginx restart',
+  path        => 'usr/bin/:/bin/'
+}
