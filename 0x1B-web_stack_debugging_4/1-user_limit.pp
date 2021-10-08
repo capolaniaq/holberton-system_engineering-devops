460021c4a78cf70418ce3e2d1545619c910878b0
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-10-07 23:23:39 -0500

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,6 @@
 # change the limits in users
 exec { 'hard--soft':
-  environment => ['hard_old=nofile 5',
-  				'hard_new=nofile 30000',
-				'soft_old=nofile 4',
-				'soft_new=nofile 10000'],
-  command     => 'sudo sed -i "s/$hard_old/$hard_new/" /etc/security/limits.conf; sudo sed -i "s/$soft_old/$soft_new/" /etc/security/limits.conf',
+  environment => ['h_old=nofile 5', 'h_new=nofile 30000', 's_old=nofile 4','s_new=nofile 10000','dir=/etc/security/limits.conf'],
+  command     => 'sudo sed -i "s/$h_old/$h_new/" $dir; sudo sed -i "s/$s_old/$s_new/" $dir',
   path        => ['/usr/bin', '/bin'],
 }
